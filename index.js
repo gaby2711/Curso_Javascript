a7b0a11c4fe109e20dc5a4f2f804058abce880e0
Ana Olivero <anagoliverom.2002@gmail.com>
2022-06-20 22:23:05 -0400

    Desafio complementario

---
+++ index.js
@@ -0,0 +1,20 @@
+/*Ciclo for
+
+for(let step = 0; step < 5; step++){
+    //se ejecuta 5 veces con los valores del paso 0 al 4
+    console.log("Camina un paso hacia el este", step);
+}
+console.log("se rompe el ciclo");
+*/
+//ciclo while
+
+let n = 0;
+let x = 0;
+
+
+while(n<5){
+    console.log("Exito",n);
+    n++;
+    x+=n;
+}
+console.log("Termina el ciclo");
